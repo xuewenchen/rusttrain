709b668a63db726d16c1df1a9a049a6aa3cb90e0
chenxuewen01 <chenxuewen01@bilibili.com>
2023-04-12 10:21:11 +0800

    add struct

--- structtrain/src/main.rs
+++ structtrain/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u32,
+    address: String,
+}
+
 fn main() {
+    let p1 = Person {
+        name: String::from("hello"),
+        age: 12,
+        address: String::from("world"),
+    };
+    let p2 = Person {
+        name: String::from("hello"),
+        address: p1.address.clone(),
+        ..p1
+    };
+
+    println!("p1 = {:?}", p1);
+    println!("p2 = {:?}", p2);
+
     println!("Hello, world!");
 }
