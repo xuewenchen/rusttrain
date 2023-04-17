f40023a05ec26c944659ea568598a44924a87b83
chenxuewen01 <chenxuewen01@bilibili.com>
2023-04-18 00:37:13 +0800

    add vec

---
+++ testcollection/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // vec
+    let mut a: Vec<i32> = Vec::new();
+    a.push(11);
+    a.push(12);
+    a.push(13);
+    a.push(14);
+
+    for ele in a {
+        println!("ele = {}", ele);
+    }
+
+    println!(" ========== this is just a ========= ");
+
+    // new vec use vec!
+    let mut b = vec![1, 2, 3];
+    for ele in &b {
+        println!("ele = {ele}");
+    }
+
+    // get data from vec
+    let item = b.get(10);
+    let a = match item {
+        Some(i) => i,
+        None => &0,
+    };
+    println!("a = {a}");
+    
+}
