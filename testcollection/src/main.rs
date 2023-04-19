7a9bcfad2c27a860d3ced77fa9d4e509f5948c04
chenxuewen01 <chenxuewen01@bilibili.com>
2023-04-19 19:12:04 +0800

    fix

--- testcollection/src/main.rs
+++ testcollection/src/main.rs
@@ -35,5 +35,37 @@ fn main() {
     } else {
         println!("there is noting in item");
     }
-    
+
+
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let s1 = String::from("s1");
+    let s2 = String::from("s2");
+
+    let s3 = s1 + &s2;
+    println!("s3 = {s3}");
+    let ss = &s2[..];
+    // println!("s1 = {s1}");
+    // println!("s2 = {s2}");
+
+    let s1 = String::from("hello1");
+    let s2 = String::from("hello2");
+    let s3 = String::from("hello3");
+    let s4 = format!("{s1}--{s2}--{s3}");
+    println!("s4 = {s4}");
+
+    for c in "Здравствуйте".chars() {
+        println!("{c}");
+    }
+    for c in "abc123陈学文".chars() {
+        println!("{c}");
+    }
+
 }
