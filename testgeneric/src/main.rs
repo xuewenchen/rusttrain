0c64172d86c7f1e4808ef6b7ef43f496b7b88490
chenxuewen01 <chenxuewen01@bilibili.com>
2023-04-22 00:32:48 +0800

    add find_largest_num

--- testgeneric/src/main.rs
+++ testgeneric/src/main.rs
@@ -1,19 +1,20 @@
 fn main() {
     
-    find_largest_num();
+    let list = vec![1, 2, 10, 4, 5];
+    find_largest_num(&list);
 }
 
-fn find_largest_num() {
-    let list = vec![1, 2, 10, 4, 5];
+fn find_largest_num(list: &Vec<i32>) {
+
     let mut flag_number = &list[0];
 
-    for ele in &list {
+    for ele in list {
         if ele > flag_number {
             flag_number = ele;
         }
     }
     println!("largest number is {flag_number}");
-    for ele in &list {
+    for ele in list {
         println!("number is {ele}");
     }   
 }
