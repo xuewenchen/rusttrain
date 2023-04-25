d7c753af339d320fe31bf51dd8c11d190ec761b7
chenxuewen01 <chenxuewen01@bilibili.com>
2023-04-26 07:35:20 +0800

    add some test

---
+++ adder/src/lib.rs
@@ -0,0 +1,89 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+// assert!()
+// assert_eq!()
+// assert_ne!()
+// should_panic micro
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+    #[test]
+    fn test_equal() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_not_equal() {
+        assert_ne!(1, 2, "two parameters one = {}, two = {}", 1, 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_guess() {
+        Guess::new(200);
+    }
+}
+
